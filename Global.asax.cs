6eee9e1d05f1525563916b6887b31aa6f03856ee
Tomas Guzman <42191764+ParadoxARG@users.noreply.github.com>
2018-11-27 15:57:41 -0300

    Full Deployment

---
+++ Global.asax.cs
@@ -0,0 +1,49 @@
+﻿namespace HelpDeskBot
+{
+    using System;
+    using System.Web.Http;
+    using Autofac;
+    using Dialogs;
+    using System.Configuration;
+    using Microsoft.Bot.Builder.Azure;
+    using Microsoft.Bot.Builder.Dialogs;
+    using Microsoft.Bot.Builder.Dialogs.Internals;
+    using Microsoft.Bot.Builder.Scorables;
+    using Microsoft.Bot.Connector;
+
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            var uri = new Uri(ConfigurationManager.AppSettings["DocumentDbUrl"]);
+            var key = ConfigurationManager.AppSettings["DocumentDbKey"];
+            var store = new DocumentDbBotDataStore(uri, key);
+            Conversation.UpdateContainer(
+                        builder2 =>
+                        {
+                            builder2.Register(c => store)
+                                .Keyed<IBotDataStore<BotData>>(AzureModule.Key_DataStore)
+                                .AsSelf()
+                                .SingleInstance();
+                            builder2.Register(c => new CachingBotDataStore(store, CachingBotDataStoreConsistencyPolicy.ETagBasedConsistency))
+                                .As<IBotDataStore<BotData>>()
+                                .AsSelf()
+                                .InstancePerLifetimeScope();
+                        });
+
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<SearchScorable>()
+                .As<IScorable<IActivity, double>>()
+                .InstancePerLifetimeScope();
+
+            builder.RegisterType<ShowArticleDetailsScorable>()
+                .As<IScorable<IActivity, double>>()
+                .InstancePerLifetimeScope();
+
+            builder.Update(Conversation.Container);
+        }
+    }
+}
